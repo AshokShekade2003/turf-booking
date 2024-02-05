eed6b8b9fc018072e48166319268d9b03965e99c
AshokShekade2003 <ashokshekade543@gmail.com>
2024-02-05 16:21:46 +0530

    Initial File Structure

---
+++ turfbooking/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img
+        src="https://media.istockphoto.com/id/1403780865/photo/football-field-at-sunset.jpg?s=2048x2048&w=is&k=20&c=lrMG0RbYeKrwFGoIaCLBezqFTCq_Qxm0fTkYQNYt6W4="
+        alt="imagephoto"
+        className="resp-img"
+      ></img>
+    </div>
+  );
+}
+
+export default App;
